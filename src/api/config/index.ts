4eb7952a5a9a55e12774665f74d05caf07b571cf
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-25 12:59:11 +0300

    updated URL

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: process.env.BASE_URL || `http://localhost:8080/api/items`,
+    baseURL: process.env.BASE_URL || `https://node-postgres-table.herokuapp.com/api/items`,
 });
